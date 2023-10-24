b97566a5bc3bc88f4fac147853afde56c2eb610b
Minh Khang <94234570+khanglag@users.noreply.github.com>
2023-10-24 21:55:21 +0700

    ok

---
+++ MiniMarket/src/main/java/com/mycompany/minimarket/MiniMarket.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.minimarket;
+
+/**
+ *
+ * @author khang
+ */
+public class MiniMarket {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
