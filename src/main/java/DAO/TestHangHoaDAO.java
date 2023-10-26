3b38583d49191144e7e9550832b1ecef5e03993d
kecuonq71 <kecuong71@gmail.com>
2023-10-26 22:52:41 +0700

    ..

---
+++ src/main/java/DAO/TestHangHoaDAO.java
@@ -0,0 +1,17 @@
+package DAO;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import DAO.NhanVienDAO;
+import DTO.ChiTietHoaDon_DTO;
+import DTO.HoaDon_DTO;
+import DTO.NhaCungCap_DTO;
+import DTO.NhanVien_DTO;
+
+public class TestHangHoaDAO {
+    public static void main(String[] args) {
+        KhachHangDAO dAO= new KhachHangDAO();
+        System.out.println(dAO.ttKhachHang("0358808913"));
+    }
+}
