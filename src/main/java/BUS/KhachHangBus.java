256678716f56b4e871d9003d0ee7d898e8a8477b
minnfeed <93919572+minnfeed@users.noreply.github.com>
2023-10-27 21:50:02 +0700

    Đức mẹ thiên tài
    Check ngày sinh, nhỏ hơn 100 tuổi, trước ngày hiện tại

--- src/main/java/BUS/KhachHangBus.java
+++ src/main/java/BUS/KhachHangBus.java
@@ -6,10 +6,12 @@ package BUS;
 
 import DAO.KhachHangDAO;
 import DTO.KhachHang_DTO;
+import Exception.DateOfBirthValidator;
+import Exception.InvalidDateOfBirthException;
 
 import Handle.Convert;
-import PhoneException.InvalidPhoneNumberException;
-import PhoneException.PhoneNumberValidator;
+import Exception.InvalidPhoneNumberException;
+import Exception.PhoneNumberValidator;
 import java.time.LocalDate;
 import javax.swing.JOptionPane;
 
@@ -18,24 +20,35 @@ import javax.swing.JOptionPane;
  * @author pc
  */
 public class KhachHangBus {
-    public KhachHangBus(){}
-    KhachHangDAO dAO=new KhachHangDAO();
-    public boolean themKhanhHang( String tenKH, LocalDate ngaySinh, String sdt, String diaChi, boolean tonTai){
+    public KhachHangBus() {
+    }
+
+    KhachHangDAO dAO = new KhachHangDAO();
+
+    public boolean themKhanhHang(String tenKH, LocalDate ngaySinh, String sdt, String diaChi, boolean tonTai) {
         PhoneNumberValidator validator = new PhoneNumberValidator();
         try {
             validator.validatePhoneNumber(sdt);
             if (dAO.ttKhachHang(sdt)) {
-            JOptionPane.showMessageDialog(null,
-                    "Đã có khách hàng sử dụng số điện thoại: "+sdt+". Vui lòng sử dụng số khách");
-            return false;
-        }
-        else{
-            String maKH=Convert.convertMa(dAO.demKhachHang()+1);
-            KhachHang_DTO kh=new KhachHang_DTO(maKH, tenKH, ngaySinh, sdt, diaChi, tonTai);
-            dAO.add(kh);
-        }
+                JOptionPane.showMessageDialog(null,
+                        "Đã có khách hàng sử dụng số điện thoại: " + sdt + ". Vui lòng sử dụng số khách");
+                return false;
+            } else {
+                try {
+                    DateOfBirthValidator dayofb= new DateOfBirthValidator();
+                    dayofb.validateDateOfBirth(ngaySinh);
+                    String maKH = Convert.convertMa(dAO.demKhachHang() + 1);
+                    KhachHang_DTO kh = new KhachHang_DTO(maKH, tenKH, ngaySinh, sdt, diaChi, tonTai);
+                    dAO.add(kh);
+                } catch (InvalidDateOfBirthException e) {
+                    JOptionPane.showMessageDialog(null,
+                    e.getMessage());
+                }
+                
+            }
         } catch (InvalidPhoneNumberException e) {
-            System.out.println("Lỗi: " + e.getMessage());
+            JOptionPane.showMessageDialog(null,
+                    e.getMessage());
         }
         return true;
     }
