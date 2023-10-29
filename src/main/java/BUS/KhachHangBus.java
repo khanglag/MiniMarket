d8ae3194903181f336edeab3f9d6245123aee309
minnfeed <93919572+minnfeed@users.noreply.github.com>
2023-10-29 23:23:25 +0700

    hehe

--- src/main/java/BUS/KhachHangBus.java
+++ src/main/java/BUS/KhachHangBus.java
@@ -14,6 +14,7 @@ import Exception.InvalidPhoneNumberException;
 import Exception.PhoneNumberValidator;
 import Handle.Convert;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 /**
@@ -25,7 +26,9 @@ public class KhachHangBus {
     }
 
     KhachHangDAO dAO = new KhachHangDAO();
-
+    public ArrayList<KhachHang_DTO> dsKhachHang(){
+        return dAO.ReadKhachHang();
+    }
     public boolean themKhanhHang(String tenKH, LocalDate ngaySinh, String sdt, String diaChi, boolean tonTai) {
         PhoneNumberValidator validator = new PhoneNumberValidator();
         //Ngoại lệ số điện thoại
--- src/main/java/BUS/Test.java
+++ src/main/java/BUS/Test.java
@@ -12,8 +12,6 @@ import java.time.LocalDate;
  */
 public class Test {
     public static void main(String[] args) {
-        HangHoaBus bus =new HangHoaBus();
-        bus.timHangHoa("", null, null, 0, null);
         KhachHangBus bus=new KhachHangBus();
         bus.themKhanhHang("Lê Duy Khang", LocalDate.now(), "0673735351", "277 Âu Dương Lân", true);
     }
