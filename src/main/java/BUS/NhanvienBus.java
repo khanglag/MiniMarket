95b08c45b57183d132cf770cf26f099231d0571b
minnfeed <93919572+minnfeed@users.noreply.github.com>
2023-10-30 23:07:52 +0700

    Huhu

--- src/main/java/BUS/NhanvienBus.java
+++ src/main/java/BUS/NhanvienBus.java
@@ -1,51 +1,62 @@
- /*
-  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-  */
- package BUS;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BUS;
+
 import DAO.NhanVienDAO;
 import DTO.NhanVien_DTO;
 import static Handle.Convert.convertMa;
 import java.time.LocalDate;
- import java.util.ArrayList;
- /**
-  *
-  * @author pc
-  */
- public class NhanvienBus {
+import java.util.ArrayList;
+
+/**
+ *
+ * @author pc
+ */
+public class NhanvienBus {
 
     public NhanvienBus() {
     }
-     NhanVienDAO dAO=new NhanVienDAO();
-    public ArrayList<NhanVien_DTO> dsNhanVien(){
+
+    NhanVienDAO dAO = new NhanVienDAO();
+
+    public ArrayList<NhanVien_DTO> dsNhanVien() {
         return dAO.ReadNhanviens();
     }
-    public boolean xoaNhanVien(String maNV){
+
+    public boolean xoaNhanVien(String maNV) {
         return dAO.delete(maNV);
     }
-    public boolean suaNhanVien(String maNV,String sdt,String diaChi, String maPQ,String email){
-         return dAO.update(maNV, sdt, diaChi, maPQ, email);
+
+    public boolean suaNhanVien(String maNV, String sdt, String diaChi, String maPQ, String email) {
+        return dAO.update(maNV, sdt, diaChi, maPQ, email);
     }
-    public boolean themNhanVien(String tenNV, LocalDate ngaySinh, String gioiTinh,String sdt,String cccd,String email, String diaChi,String maPQ){
-        NhanVien_DTO dTO=new NhanVien_DTO(taoMaNV(sdt, maPQ), tenNV, ngaySinh, gioiTinh, cccd, sdt, email, diaChi, maPQ, true);
+
+    public boolean themNhanVien(String tenNV, LocalDate ngaySinh, String gioiTinh, String sdt, String cccd,
+            String email, String diaChi, String maPQ) {
+        NhanVien_DTO dTO = new NhanVien_DTO(taoMaNV(sdt, maPQ), tenNV, ngaySinh, gioiTinh, cccd, sdt, email, diaChi,
+                maPQ, true);
         return dAO.add(dTO);
     }
-    public String taoMaNV(String sdt,String maPQ){
-        int startIndex = sdt.length()-6;
-        String ma="";
-        String subsdt=sdt.substring(startIndex);
+
+    public String taoMaNV(String sdt, String maPQ) {
+        int startIndex = sdt.length() - 6;
+        String ma = "";
+        String subsdt = sdt.substring(startIndex);
         switch (maPQ) {
             case "NVBH203":
             case "NVTK203":
-                ma+="A";
+                ma += "A";
                 break;
-            default: ma+="B";
+            default:
+                ma += "B";
         }
-        while(dAO.ttMaNV(ma+subsdt)==true){
-            subsdt= convertMa(Integer.parseInt(subsdt)+1);
-            subsdt=subsdt.substring(1);
-            System.out.println("sub sdt"+subsdt);
+        while (dAO.ttMaNV(ma + subsdt) == true) {
+            subsdt = convertMa(Integer.parseInt(subsdt) + 1);
+            subsdt = subsdt.substring(1);
+            System.out.println("sub sdt" + subsdt);
         }
-        return (ma+=subsdt);
+        return (ma += subsdt);
     }
- }
+}
--- src/main/java/BUS/Test.java
+++ src/main/java/BUS/Test.java
@@ -15,5 +15,6 @@ public class Test {
     public static void main(String[] args) {
         NhanvienBus bus = new NhanvienBus();
         bus.themNhanVien("Le Duy Khang",Timeconvert.convert("04-10-2003"), "nam", "0358808913", "091203002454","lehasdhqwdjabwsdha@com", "277 âu dương lân", "NVTK203");
+        ChiTietPhieuXuatBus phieuXuatBus = new ChiTietPhieuXuatBus();
     }
 }
