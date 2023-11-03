5e9146bcc00c3e67b4018e3b1510179f14799b15
Minh Khang <94234570+khanglag@users.noreply.github.com>
2023-11-03 23:29:07 +0700

    o

---
+++ src/main/java/BUS/PhieuYeuCauNhapBus.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BUS;
+
+import DAO.PhieuYeuCauNhapDAO;
+import DTO.ChiTietPhieuNhap_DTO;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author khang
+ */
+public class PhieuYeuCauNhapBus {
+    private final PhieuYeuCauNhapDAO ctnDAO;
+
+    public PhieuYeuCauNhapBus() {
+        ctnDAO = new PhieuYeuCauNhapDAO();
+    }
+    public ArrayList<ChiTietPhieuNhap_DTO> rChiTietPhieuNhap_DTOs() {
+        try {
+            return ctnDAO.readBD();
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Lỗi khi đọc dữ liệu !!!!", "Lỗi", JOptionPane.ERROR_MESSAGE);
+            return new ArrayList<>();
+        }
+    }
+
+    public boolean themCTPN(ChiTietPhieuNhap_DTO ctn) {
+        try {
+            if (ctn.getMaPhieuNhap() == null || ctn.getMaHangNhap() == null || ctn.getTenHangNhap() == null || ctn.getMaNCC() == null || ctn.getVAT() < 0 || ctn.getXuatXu() == null || ctn.getAnhSP() == null || ctn.getSoLuong() < 0 || ctn.getDonVi() == null || ctn.getGiaNhap() < 0 || ctn.getTongTienNhap() < 0) {
+                throw new IllegalArgumentException("Dữ liệu không hợp lệ");
+            }
+    
+            if (ctnDAO.isCTPN_Existed(ctn.getMaPhieuNhap(), ctn.getMaHangNhap())) {
+                return false;
+            }
+    
+            ChiTietPhieuNhap_DTO ct = new ChiTietPhieuNhap_DTO(ctn.getMaPhieuNhap(), ctn.getMaHangNhap(),ctn.getTenHangNhap(),ctn.getMaNCC(),ctn.getVAT(),ctn.getXuatXu(),ctn.getAnhSP(),ctn.getSoLuong(),ctn.getDonVi(),ctn.getGiaNhap(),ctn.getTongTienNhap(),true);
+    
+            if (ctnDAO.addCTPN(ct)) {
+                return true;
+            } else {
+                throw new Exception("Thêm Chi Tiết Phiếu Nhập thất bại");
+            }
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Dữ liệu không hợp lệ.", "Lỗi", JOptionPane.WARNING_MESSAGE);
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Lỗi khi thêm Chi Tiết Phiếu Nhập!!!", "Lỗi", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+    }
+    
+   
+
+
+    public boolean xoaCTPN(ChiTietPhieuNhap_DTO ct) {
+        try {
+            if (ctnDAO.isCTPN_Existed(ct.getMaPhieuNhap(), ct.getMaHangNhap())) {
+                return ctnDAO.deleteCTPN(ct);
+            } else {
+                JOptionPane.showMessageDialog(null, "Sản phẩm này không tồn tại trong phiếu", "Lỗi", JOptionPane.WARNING_MESSAGE);
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Lỗi khi xóa Phiếu!!!", "Lỗi", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+    }
+
+    public boolean suaCTPN(ChiTietPhieuNhap_DTO ct) {
+        try {
+             if (ctnDAO.isCTPN_Existed(ct.getMaPhieuNhap(), ct.getMaHangNhap())) {
+                return ctnDAO.updateCTPN(ct);
+            } else {
+                JOptionPane.showMessageDialog(null, "Sản phẩm này không tồn tại trong phiếu", "Lỗi", JOptionPane.WARNING_MESSAGE);
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Lỗi khi sửa thông tin Phiếu!!!", "Lỗi", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+    }
+
+    public ArrayList<ChiTietPhieuNhap_DTO> timCTPN(String maPhieuNhap,String maHangNhap, String tenHangNhap, String xuatXu) {
+        try {
+            return ctnDAO.searchCTPN(maPhieuNhap, maHangNhap, tenHangNhap, xuatXu);
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Lỗi khi tìm Kiếm Phiếu !!!!", "Lỗi", JOptionPane.ERROR_MESSAGE);
+            return new ArrayList<>();
+        }
+    }
+}
---
+++ src/main/java/DAO/PhieuYeuCauNhapDAO.java
@@ -0,0 +1,161 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import ConnectDB.ConnectDB;
+import DTO.ChiTietPhieuNhap_DTO;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author khang
+ */
+public class PhieuYeuCauNhapDAO {
+     public ArrayList<ChiTietPhieuNhap_DTO> readBD() {
+        ConnectDB connectBD = new ConnectDB();
+        ArrayList<ChiTietPhieuNhap_DTO> chiTietPhieuNhap_DTOs = new ArrayList<>();
+        String qry = "SELECT * FROM `phieuyeucaunhap` WHERE TONTAI = 1";
+        ResultSet rs = null;
+        try {
+            rs = connectBD.sqlQuery(qry);
+            if (rs != null) {
+                while (rs.next()) {
+                    ChiTietPhieuNhap_DTO ctpn = new ChiTietPhieuNhap_DTO(
+                            rs.getNString("MAPHIEUNHAP"),
+                            rs.getNString("MAHANGNHAP"),
+                            rs.getNString("TENHANGNHAP"),
+                            rs.getString("MANCC"),
+                            rs.getDouble("VAT"),
+                            rs.getNString("XUATXU"),
+                            rs.getNString("ANHSP"),
+                            rs.getInt("SOLUONG"),
+                            rs.getNString("DONVI"),
+                            rs.getDouble("GIANHAP"),
+                            rs.getDouble("TONGTIENNHAP"),
+                            rs.getBoolean("TONTAI"));
+                    chiTietPhieuNhap_DTOs.add(ctpn);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Lỗi truy vấn !!!");
+            e.printStackTrace();
+        }
+        return chiTietPhieuNhap_DTOs;
+    }
+
+    public boolean addCTPN(ChiTietPhieuNhap_DTO ctpn) {
+        boolean kq = false;
+        ConnectDB connectBD = new ConnectDB();
+        kq = connectBD.sqlUpdate(""
+                + "INSERT INTO `phieuyeucaunhap` (`MAPHIEUNHAP`, `MAHANGNHAP`, `TENHANGNHAP`, `MANCC` , `VAT`, `XUATXU`, `ANHSP`, `SOLUONG`, `DONVI`, `GIANHAP`, `TONGTIENNHAP`, `TONTAI`) VALUES ("
+                + "'" + ctpn.getMaPhieuNhap() + "',"
+                + "'" + ctpn.getMaHangNhap() + "',"
+                + "'" + ctpn.getTenHangNhap() + "',"
+                + "'" + ctpn.getMaNCC() + "',"
+                + "'" + ctpn.getVAT() + "',"
+                + "'" + ctpn.getXuatXu() + "',"
+                + "'" + ctpn.getAnhSP() + "',"
+                + "'" + ctpn.getSoLuong() + "',"
+                + "'" + ctpn.getDonVi() + "',"
+                + "'" + ctpn.getGiaNhap() + "',"
+                + "'" + ctpn.getTongTienNhap() + "',"
+                + "'1')");
+        connectBD.closeConnect();
+        return kq;
+    }
+
+    public boolean deleteCTPN(ChiTietPhieuNhap_DTO ctpn) {
+        boolean success = false;
+        ConnectDB connectDB = new ConnectDB();
+        String sql = "UPDATE `phieuyeucaunhap` SET TONTAI = 0 WHERE `MAPHIEUNHAP` = '" + ctpn.getMaPhieuNhap()
+                + "' AND `MAHANGNHAP` = '" + ctpn.getMaHangNhap() + "'";
+        success = connectDB.sqlUpdate(sql);
+        connectDB.closeConnect();
+        return success;
+    }
+
+    public boolean updateCTPN(ChiTietPhieuNhap_DTO ctpn) {
+        ConnectDB connectDB = new ConnectDB();
+        boolean kq = connectDB.sqlUpdate("UPDATE `phieuyeucaunhap` SET"
+                + " TENHANGNHAP = '" + ctpn.getTenHangNhap() + "',"
+                + " XUATXU = '" + ctpn.getXuatXu() + "',"
+                + " ANHSP = '" + ctpn.getAnhSP() + "',"
+                + " SOLUONG = '" + ctpn.getSoLuong() + "',"
+                + " GIANHAP = '" + ctpn.getGiaNhap() + "',"
+                + " TONGTIENNHAP = '" + ctpn.getTongTienNhap() + "'"
+                + " WHERE `MAPHIEUNHAP` = '" + ctpn.getMaPhieuNhap() + "' AND `MAHANGNHAP` = '" + ctpn.getMaHangNhap()
+                + "'");
+        connectDB.closeConnect();
+        return kq;
+    }
+
+    public ArrayList<ChiTietPhieuNhap_DTO> searchCTPN(String maPhieuNhap, String maHangNhap, String tenHangNhap,String xuatXu) {
+        ArrayList<ChiTietPhieuNhap_DTO> ds = new ArrayList<>();
+        ConnectDB connectDB = new ConnectDB();
+        StringBuilder qry = new StringBuilder("SELECT * FROM `phieuyeucaunhap` WHERE TONTAI = 1");
+
+        if (maPhieuNhap != null && !maPhieuNhap.isEmpty()) {
+            qry.append(" AND `MAPHIEUNHAP` =  '" + maPhieuNhap + "'");
+        }
+
+        if (maHangNhap != null && !maHangNhap.isEmpty()) {
+            qry.append(" AND `MAHANGNHAP` =  '" + maHangNhap + "'");
+        }
+
+        if (tenHangNhap != null && !tenHangNhap.isEmpty()) {
+            qry.append(" AND `TENHANGNHAP` LIKE '%" + tenHangNhap + "%'");
+        }
+        if (xuatXu != null && !xuatXu.isEmpty()) {
+            qry.append(" AND `XUATXU` LIKE '%" + xuatXu + "%'");
+        }
+
+        ResultSet rs = connectDB.sqlQuery(qry.toString());
+
+        try {
+            if (rs != null) {
+                while (rs.next()) {
+                    ChiTietPhieuNhap_DTO ctpn = new ChiTietPhieuNhap_DTO(
+                            rs.getNString("MAPHIEUNHAP"),
+                            rs.getNString("MAHANGNHAP"),
+                            rs.getNString("TENHANGNHAP"),
+                            rs.getString("MANCC"),
+                            rs.getDouble("VAT"),
+                            rs.getNString("XUATXU"),
+                            rs.getNString("ANHSP"),
+                            rs.getInt("SOLUONG"),
+                            rs.getNString("DONVI"),
+                            rs.getDouble("GIANHAP"),
+                            rs.getDouble("TONGTIENNHAP"),
+                            rs.getBoolean("TONTAI"));
+
+                    ds.add(ctpn);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ds;
+
+    }
+    public boolean isCTPN_Existed(String maPhieu, String maSP) {
+        ConnectDB connectDB = new ConnectDB();
+        String qry = "SELECT 1 FROM `phieuyeucaunhap` WHERE `MAPHIEUNHAP` = '" + maPhieu + "' AND `MAHANGNHAP` = '" + maSP + "'";
+        
+        ResultSet rSet = connectDB.sqlQuery(qry);
+        
+        if (rSet != null) {
+            try {
+                return rSet.next(); 
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        return false; 
+    }
+
+}
